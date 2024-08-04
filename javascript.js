1df329dbd348152083c43150d6b47768c93837bf
farelrojas <farelrojas@yahoo.es>
2024-08-04 22:09:27 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
 // Add event listener to go back button
 document.getElementById('go-back').addEventListener('click', () => {
-  window.location.href = 'learning_options.html';
+  window.location.href = 'index.html';
 });
